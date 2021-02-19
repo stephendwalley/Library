c1c3ce8578dae99dcb381527b5aaed1d8270d2b1
stephendwalley <76471970+stephendwalley@users.noreply.github.com>
2021-02-19 18:01:29 +1100

    Skelton created

---
+++ index.js
@@ -0,0 +1,24 @@
+let myLibrary = [];
+
+function Book(title, author, pages, read) {
+	this.title = title;
+	this.author = author;
+	this.pages = pages;
+	this.read = read;
+	this.info = function () {
+		return title + " by " + author + ", " + pages + " pages, " + read;
+	};
+}
+
+function addBookToLibrary() {
+    
+}
+
+const theHobbit = new Book(
+	"The Hobbit",
+	"J.R.R. Tolkien",
+	"295",
+	"not read yet"
+);
+
+console.log(theHobbit.info());
